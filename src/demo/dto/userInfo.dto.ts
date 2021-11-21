bc03b24241e026e19bba821b4fe3313ee6a42da5
wynn-w <wwwynn22@gmail.com>
2021-11-21 19:43:15 +0800

    feat: 完成 userInfo 的传输对象创建

---
+++ src/demo/dto/userInfo.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserDTO {
+  @ApiProperty({ example: 10001, description: 'id', required: true })
+  @IsNotEmpty({ message: 'id 不能为空' })
+  @IsNumber()
+  id!: number;
+  @ApiProperty({ example: 'xiaoming', description: 'name', required: false })
+  @IsString()
+  name!: string;
+}
